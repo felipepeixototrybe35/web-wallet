da25aa5983bde8d5b8e3ad36830daaea87e57f2c
Felipe-33 <fepepiuerj@gmail.com>
2023-12-03 20:14:38 -0300

    Inicindo requisito 3

--- src/components/WalletForm.tsx
+++ src/components/WalletForm.tsx
@@ -1,5 +1,33 @@
 function WalletForm() {
-  return <div>WalletForm</div>;
+  return (
+    <form>
+      <input
+        type="text"
+        placeholder="despesa-valor"
+        data-testid="value-input"
+      />
+      <input
+        type="text"
+        placeholder="despesa-descrição"
+        data-testid="description-input"
+      />
+      <select
+        data-testid="currency-input"
+      >
+        moeda
+      </select>
+      <select
+        data-testid="method-input"
+      >
+        pagamento
+      </select>
+      <select
+        data-testid="tag-input"
+      >
+        categoria
+      </select>
+    </form>
+  );
 }
 
 export default WalletForm;
